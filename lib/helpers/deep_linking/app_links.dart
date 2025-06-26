022ca650e281a0e3e1dd7a73f430944b7684883b
ahmed emara <emara8028@gmail.com>
2025-06-26 21:04:16 +0300

    make deep linking config and testing

--- lib/helpers/deep_linking/app_links.dart
+++ lib/helpers/deep_linking/app_links.dart
@@ -34,3 +34,66 @@ class AppLinksImpl extends DeepLinkingService{
     });
   }
 }
+
+/*
+Android:
+in backend server should be set up : assetlinks.json
+- Upload the assetlinks.json file to your website. It must be accessible via https://yourdomain/.well-known/assetlinks.json
+
+You can generate your app's SHA256 fingerprint with this command:
+keytool -list -v -keystore <keystore path> -alias <key alias> -storepass <store password> -keypass <key password>
+
+[{
+  "relation": ["delegate_permission/common.handle_all_urls"],
+  "target": {
+    "namespace": "android_app",
+    "package_name": "com.example",
+    "sha256_cert_fingerprints":
+    ["00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00:00"]
+  }
+}]
+
+IOS:
+Create a new file named apple-app-site-association with no file extension.
+{
+  "applinks":{
+    "apps":[
+
+    ],
+    "details":[
+      {
+        "appIDs":[
+          "TeamID.BundleID"
+        ],
+        "components":[
+          {
+            "/":"/login",
+            "comment":"Matches URL with a path /login"
+          },
+          {
+            "/":"/product/*",
+            "comment":"Matches any URL with a path that starts with /product/."
+          },
+          {
+            "/":"/secret",
+            "exclude":true,
+            "comment":"Matches URL with a path /secret and instructs the system not to open it as a universal link."
+          }
+        ]
+      }
+    ]
+  }
+}
+
+* test deep link on Android
+* adb shell am start -a android.intent.action.VIEW \
+  -c android.intent.category.BROWSABLE \
+  -d https://yourDomain.com \
+  <package name>
+
+  test deep link on IOS:
+  xcrun simctl openurl booted https://yourDomain.com/path
+
+  * test - client server integration : https://developers.google.com/digital-asset-links/tools/generator
+  */
+ */
