47428fe1e430281bbe3804c7f73c0072329dfc71
Ahmed Emara <118583155+ahmedemara231@users.noreply.github.com>
2025-08-03 23:05:14 +0300

    Update  internet_interceptor_test.dart

--- lib/widgets/internet_supporter_widget/test.dart
+++ lib/widgets/internet_supporter_widget/test.dart
@@ -1,8 +1,11 @@
 import 'dart:developer';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:helper_repo/widgets/internet_supporter_widget/internet_supporter_widget.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import '../text.dart';
 
 class InternetSupporterTest extends StatelessWidget {
   const InternetSupporterTest({super.key});
@@ -10,15 +13,16 @@ class InternetSupporterTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: InternetSupporterWidget(
-          onChanged: (connectionStatus) => log('the status is changed $connectionStatus'),
-          onInitialStatusBuilder: (connectionStatus) =>
-              Center(child: Text('initial status $connectionStatus')),
-          builder: (connectionStatus) => Center(
-            child: connectionStatus == InternetConnectionStatus.connected ?
-            const Text('connected') : const Text('disconnected'),
-          ),
-      ),
+      body: Center(
+        child: InternetInterceptorWidget(
+          loadingWidget: CupertinoActivityIndicator(),
+          onChanged: (status) => status == InternetConnectionStatus.disconnected,
+          builder: (status) => status == InternetConnectionStatus.disconnected?
+          AppText('not connected') : AppText('connected'),
+          onInitialStatusBuilder: (status) =>  status == InternetConnectionStatus.disconnected?
+          AppText(' onInitialStatusBuilder not connected') : AppText(' onInitialStatusBuilder connected'),
+        ),
+      )
     );
   }
 }
